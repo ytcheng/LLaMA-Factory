e1c661a69d96c79ac4f49e8595c4a03d1a040d1a
ytcheng <ytcheng@qq.com>
2024-05-12 08:54:15 +0000

    augment

--- preprocessing/augment_pretrain_dataset.py
+++ preprocessing/augment_pretrain_dataset.py
@@ -29,7 +29,7 @@ def augment(content):
 
     generated_ids = model.generate(
         model_inputs.input_ids,
-        max_new_tokens=512
+        max_new_tokens=2048
     )
     generated_ids = [
         output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
