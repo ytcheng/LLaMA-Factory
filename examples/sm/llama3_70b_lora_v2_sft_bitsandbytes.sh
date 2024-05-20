7e703732185830588f3b23208d81b57efb0805fb
ytcheng <ytcheng@qq.com>
2024-05-20 05:41:17 +0000

    70b

---
+++ examples/sm/llama3_70b_lora_v2_sft_bitsandbytes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=0,1 accelerate launch \
+    --config_file examples/accelerate/single_config.yaml \
+    src/train.py examples/sm/llama3_70b_lora_v2_sft_bitsandbytes.yaml
